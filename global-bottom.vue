ab4e2b9640cfc80fef5b3f77cf92adab4f865156
asm0dey <pavel.finkelshtein@gmail.com>
2025-12-03 15:06:46 +0100

    update

---
+++ global-bottom.vue
@@ -0,0 +1,4 @@
+<template>
+    <footer v-if="[3, 4, 9, 35, 56].indexOf($slidev.nav.currentPage) == -1" class="absolute bottom-0 left-0 right-0 p-2">
+        Pasha Finkelshteyn <logos-bluesky /> @asm0dey.site <logos-twitter /> @asm0di0 </footer>
+</template>
